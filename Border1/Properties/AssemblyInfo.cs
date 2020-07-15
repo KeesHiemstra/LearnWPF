0b2afa3345276f1520d325d73eaff56bd6a9a079
Kees Hiemstra <chi@xs4all.nl>
2020-07-15 12:07:25 +0200

    Added a README.md.

--- Border1/Properties/AssemblyInfo.cs
+++ Border1/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Border1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Experiments")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Border1")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("CHi")]
+[assembly: AssemblyProduct("LearnPWF")]
+[assembly: AssemblyCopyright("Copyright © Kees Hiemstra 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
---
+++ Border1/README.md
@@ -0,0 +1,7 @@
+﻿# Border1
+
+*Origin:* __Games/Letter__
+
+The border is collections of borders, but acting on a mouse action was hard to find.
+
+This experiment shows the differences between predefined border and programmable border.
